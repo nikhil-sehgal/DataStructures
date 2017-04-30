5b8ef6e6916b1a1dc17d89f8d40cd8e65b619f22
nikhil sehgal <nikhilsehgal13@gmail.com>
2017-04-30 01:16:44 -0400

    Added comments

--- src/Strings/StringCamelCase.java
+++ src/Strings/StringCamelCase.java
@@ -1,5 +1,18 @@
 package Strings;
-
+/*Objective is to find number of words in given sentence
+ * The sentence is in camel case so after first word all the words 
+ * will start from upper case.
+ * Example : 
+ * 	There are 6 words in "letsFindTheNumberOfWords."
+ * 1.lets
+ * 2.Find
+ * 3.The
+ * 4.Number
+ * 5.Of
+ * 6.Words.
+ * 
+ *  So output will be 6.* 
+ * */
 public class StringCamelCase {
 	
 	public static int camelCase(String string)
